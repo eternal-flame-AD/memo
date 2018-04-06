64779449ed777680e8326ba96c846951b66c23aa
_eternal_flame <35571479+eternal-flame-AD@users.noreply.github.com>
2018-04-06 16:49:10 +0800

    fix coveralls finally

--- test_singleton.py
+++ test_singleton.py
@@ -1,11 +1,13 @@
-from singleton import MyClass,MySingletonClass,MyNonSingletonClass
+from singleton import MyClass, MySingletonClass, MyNonSingletonClass
+
+
 def test_singleton():
-    a=MyClass()
-    b=MyClass()
-    assert a!=b
-    a=MySingletonClass()
-    b=MySingletonClass()
-    assert a==b
-    a=MyNonSingletonClass()
-    b=MyNonSingletonClass()
-    assert a!=b
+    a = MyClass()
+    b = MyClass()
+    assert a != b
+    a = MySingletonClass()
+    b = MySingletonClass()
+    assert a == b
+    a = MyNonSingletonClass()
+    b = MyNonSingletonClass()
+    assert a != b
