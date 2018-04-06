1735d81dbe92235e4fd1a96a1993009e8481cda2
_eternal_flame <35571479+eternal-flame-AD@users.noreply.github.com>
2018-04-06 17:32:52 +0800

    put test into one folder

--- tests/test_csv_read.py
+++ tests/test_csv_read.py
@@ -1,6 +1,11 @@
+import os,sys  
+
+
+parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  
+sys.path.insert(0,parentdir) 
 def test_csv_read():
     import csv_read
     expectedResult = {'Hello': 'bonjour', 'Welcome': 'bienvenue'}
-    data = csv_read.readcsv('test_csv.csv')
+    data = csv_read.readcsv('tests/test_csv.csv')
     for row in data:
         assert expectedResult[row['English']] == row['French']
